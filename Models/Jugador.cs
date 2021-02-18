d6e618c15b4df42e828c1cd1e42ad5bce7b8242a
anzucel <andreazucely70@gmail.com>
2021-02-18 00:04:06 -0600

    Se agregó clase "Jugador"

---
+++ Models/Jugador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_EDI
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public string apellido { get; set; }
+        public string posicion { get; set; }
+        public int salario { get; set; }
+        public string club { get; set; }
+    }
+}
