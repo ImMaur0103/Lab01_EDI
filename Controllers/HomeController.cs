82580af8f7fede865d4ed8d5ac98fef53b027f08
anzucel <andreazucely70@gmail.com>
2021-02-19 12:31:29 -0600

    Se añadió la vista Add al controlador

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -59,6 +59,13 @@ namespace Lab01_EDI.Controllers
             return View();
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
