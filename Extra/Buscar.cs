928bce447fcbcb278c87d369964d30b41d33b8ce
anzucel <andreazucely70@gmail.com>
2021-02-19 22:47:11 -0600

    Se creó método de búsqueda secuencial dentro de la lista

--- Extra/Buscar.cs
+++ Extra/Buscar.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab01_EDI.Models;
+using Lab01_EDI.ListaDobleArtesanal;
 
 namespace Lab01_EDI.Extra
 {
 
     public class Buscar
     {
+        static ListaDoble<Jugador> ListaAux = new ListaDoble<Jugador>();
 
+        public void BuscarElemento(string OBuscar, ListaDoble<Jugador> JugadorLista)
+        {
+            int contador = JugadorLista.contador;
+
+            for (int i = 0; i < contador; i++)
+            {
+                Jugador jugador = new Jugador();
+                jugador = JugadorLista.ObtenerValor(i);
+                
+
+                if (jugador.Nombre == OBuscar)
+                {
+                    ListaAux.InsertarInicio(jugador);
+                }
+            }
+        }
     }
 
    
